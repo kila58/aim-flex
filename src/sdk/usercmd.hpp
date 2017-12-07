1020e17441e5697d2c69f81c9f3ba63478fd966b
kila58 (Gage) <kilacow@gmail.com>
2017-12-07 10:52:10 -0500

    fixed ugly cusercmd formatting

--- src/sdk/usercmd.hpp
+++ src/sdk/usercmd.hpp
@@ -5,17 +5,17 @@ class CUserCmd
 public:
 	virtual ~CUserCmd();
 
-	int		command_number;
-	int		tick_count;
-	Vector	viewangles; //make angle type
+	int command_number;
+	int tick_count;
+	Vector viewangles; //make angle type
 	Vector	aimdirection;
 	Vector	move;
-	int		buttons;
-	byte    impulse;
-	int		weaponselect;
-	int		weaponsubtype;
-	int		random_seed;
-	short	mousedx;
-	short	mousedy;
-	bool	hasbeenpredicted;
+	int	buttons;
+	byte impulse;
+	int weaponselect;
+	int weaponsubtype;
+	int random_seed;
+	short mousedx;
+	short mousedy;
+	bool hasbeenpredicted;
 };
